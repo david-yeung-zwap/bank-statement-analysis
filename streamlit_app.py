3dd0782daf0ee0f3d551899a1d954e9ff0573303
david-yeung-zwap <david.yeung.zwap@gmail.com>
2025-04-22 12:47:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from utils.model import process_file_with_gemini
 
 def process_file_and_keyword(file: UploadedFile, keyword: list[str], api_key: str):
 
-    API_KEY: str = api_key or st.secrets["GEMINI_API_KEY"]
+    API_KEY: str = api_key
     """
     Process the uploaded file and text input.
     This is a placeholder function - replace with your actual processing logic.
@@ -35,12 +35,12 @@ uploaded_file = st.file_uploader("Choose an file...", type=["jpg", "jpeg", "png"
 
 # user_text = st.text_area("Enter your keywords with new line as separator:", height=150, placeholder="THE H K JOCKEY CLUB\n自動轉帳\nAUTOPAY")
 api_key = st.text_input("Enter API Key:", type="password")
+key = api_key or st.secrets["GEMINI_API_KEY"]
 # Add regex validation for CSV format
 # Simple CSV validation - checks if lines have consistent number of commas
 # text_list = user_text.strip().split('\n')
 # text_list = [text.strip() for text in text_list]
-
-if uploaded_file is not None and api_key:
+if uploaded_file is not None and key:
     # Process the inputs
     bytes_data = uploaded_file.getvalue()
 
@@ -52,16 +52,16 @@ if uploaded_file is not None and api_key:
         st.image(bytes_data, width=300)
 
 
+    with st.status("Processing image and text..."):
+        # Process the image and text
+        result = process_file_and_keyword(uploaded_file, [], key)
 
-    # Process the image and text
-    result = process_file_and_keyword(uploaded_file, [], api_key)
+        print(result)
+        df_result = pd.DataFrame(result['transactions'])
 
-    print(result)
-    df_result = pd.DataFrame(result['transactions'])
-
-    # Display the results
-    st.subheader("Processing Results:")
-    st.dataframe(df_result, hide_index=True)
+        # Display the results
+        st.subheader("Processing Results:")
+        st.dataframe(df_result, hide_index=True)
 
     # You can customize the output display based on your specific needs
     # For example, if your function returns a processed image:
